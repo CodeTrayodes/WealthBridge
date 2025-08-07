008e4a9db95fd406ac777e5512923f9b3bc377ec
CodeTrayodes <operations@trayodes.com>
2025-08-07 03:19:15 +0100

    bug fixfinal

--- src/components/ui/index.js
+++ src/components/ui/index.js
@@ -1,9 +1,9 @@
 // src/components/ui/index.js
 "use client"
 
-export { Button, buttonVariants } from './Button'
-export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent } from './Card'
-export { Badge, badgeVariants, StatusBadge, FeatureBadge } from './Badge'
+export { Button, buttonVariants } from './button'
+export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent } from './card'
+export { Badge, badgeVariants, StatusBadge, FeatureBadge } from './badge'
 export { LoadingSpinner } from './LoadingSpinner'
 export { GradientText } from './GradientText'
 export { AnimatedCounter } from './AnimatedCounter'
